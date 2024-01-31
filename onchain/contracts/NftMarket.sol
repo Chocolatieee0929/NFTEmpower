88b937c9036a8c1dc9cbb239e94dc7f48bedfddb
Chocolatieee0929 <luoruixx@163.com>
2024-01-31 22:37:57 +0800

    add 通过卖家签名上架以及购买nft

--- onchain/contracts/NftMarket.sol
+++ onchain/contracts/NftMarket.sol
@@ -2,12 +2,18 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Nonces.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
-import {MyNft} from "./MyNft.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-// import './Token.sol';
+import {MyNft} from "./MyNft.sol";
+import {SigUtils} from "./extension/SigUtils.sol";
 
 /**
  * @title NftMarket contract
@@ -17,98 +23,69 @@ import {MyNft} from "./MyNft.sol";
  * 1. 存储NFT的拥有者挂单信息（listingNft）
  * 2. 获得授权，作为中间人，转移 卖家nft 和 买家的erc20 token
  */
-contract NftMarket is EIP712Upgradeable, NoncesUpgradeable {
-    struct NftItemListed {
-        uint256 price;
-        address seller;
-        address erc20; // 如果为0x0，则表示eth
-    }
 
+// @custom:oz-upgrades
+contract NftMarket is ReentrancyGuard, EIP712, Nonces {
     error NftNotListing();
-    error NftHasListed();
+    error InvalidNFTAddress();
+    error InvalidSigner(address signer, address seller);
+    error ExpiredSignature(uint256 deadline);
+    error NotEnoughToken(uint256 balance, uint256 price);
+
     error TokenNotSupported();
     error TokenOrPriceNotValid();
 
     event Listing(address indexed nftAddress, uint256 tokenId, address seller, uint256 price);
     event DeList(address indexed nftAddress, uint256 tokenId);
 
-    mapping(address => mapping(uint8 => NftItemListed)) public nftsListed;
+    bytes32 private constant PERMIT_TYPEHASH =
+        keccak256("List(address owner,address nftTokenAddress,uint256 price,uint256 nonce)");
 
-    constructor() {
-        _disableInitializers();
+    address immutable tokenAddress;
+
+    constructor(address _tokenAddress) EIP712("NFTEmpower", "1") {
+        tokenAddress = _tokenAddress;
     }
 
-    function buyNft(address nftAddress, uint8 tokenId) public {
-        if (nftsListed[nftAddress][tokenId].seller == address(0)) {
-            delete nftsListed[nftAddress][tokenId];
-            revert NftNotListing();
-        }
-
-        if (nftsListed[nftAddress][tokenId].erc20 == address(0)) {
-            // eth
-            payable(nftsListed[nftAddress][tokenId].seller).transfer(nftsListed[nftAddress][tokenId].price);
-        } else {
-            // erc20
-            IERC20(nftsListed[nftAddress][tokenId].erc20).transferFrom(
-                msg.sender, nftsListed[nftAddress][tokenId].seller, nftsListed[nftAddress][tokenId].price
-            );
-        }
-
-        // nft: seller --> contract -> buyer
-        MyNft(nftAddress).transferFrom(nftsListed[nftAddress][tokenId].seller, address(this), tokenId);
-        MyNft(nftAddress).transferFrom(address(this), msg.sender, tokenId);
-
-        delete nftsListed[nftAddress][tokenId];
-        emit DeList(nftAddress, tokenId);
-    }
-
-    function listNFT(
-        address nftAddress,
-        uint8 tokenId,
-        address erc20,
-        uint256 price,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
-        if (nftsListed[nftAddress][tokenId].seller == address(0)) {
-            revert NftHasListed();
-        }
-        if (price <= 0 || isValidErc20(erc20) == false) {
-            revert TokenOrPriceNotValid();
-        }
-
-        NftItemListed memory newNftItem;
-        newNftItem.seller = msg.sender;
-        newNftItem.price = price;
-        newNftItem.erc20 = erc20;
-        nftsListed[nftAddress][tokenId] = newNftItem;
-
-        MyNft(nftAddress).permit(msg.sender, address(this), tokenId, deadline, v, r, s);
-
-        emit Listing(nftAddress, tokenId, msg.sender, price);
-
-        // MyNft(nftAddress).safeTransferFrom(msg.sender, address(this), nftTokenId, abi.encode(nftAddress));
-    }
-
-    /**
-     *  当用户取消授权（下架时调用）
-     */
-    function deListNFT(address nftAddress, uint8 tokenId) external {
-        require(nftsListed[nftAddress][tokenId].seller == msg.sender, "not the owner");
-        delete nftsListed[nftAddress][tokenId];
-        emit DeList(nftAddress, tokenId);
-    }
-
-    function recoverSig(address seller, uint8 tokenId, uint256 price, uint16 deadline, uint8 v, bytes32 r, bytes32 s)
-        external
-        pure
-        returns (address)
+    function buyNFT(address nftAddress, uint8 tokenId, uint256 price, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        nonReentrant
+        returns (bool)
     {
-        bytes32 hash = keccak256(abi.encodePacked(seller, tokenId, price, deadline));
-        address signer = ecrecover(hash, v, r, s);
-        return signer;
+        _permit(nftAddress, tokenId, price, deadline, v, r, s);
+        _buyNft(msg.sender, nftAddress, tokenId, price);
+        return true;
+    }
+
+    function _buyNft(address buyer, address nftAddress, uint8 tokenId, uint256 price) internal {
+        // nft: seller --> buyer  contract approved
+        address seller = MyNft(nftAddress).ownerOf(tokenId);
+        IERC20(tokenAddress).transferFrom(buyer, seller, price);
+        MyNft(nftAddress).safeTransferFrom(seller, buyer, tokenId);
+        emit DeList(nftAddress, tokenId);
+    }
+
+    function _permit(address nftAddress, uint8 tokenId, uint256 price, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        internal
+        returns (bool)
+    {
+        if (block.timestamp > deadline) {
+            revert ExpiredSignature(deadline);
+        }
+        if (nftAddress == address(0)) {
+            revert InvalidNFTAddress();
+        }
+        address seller = MyNft(nftAddress).ownerOf(tokenId);
+        bytes32 structHash =
+            keccak256(abi.encode(PERMIT_TYPEHASH, seller, nftAddress, price, _useNonce(address(this)), deadline));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        if (signer != seller) {
+            revert InvalidSigner(signer, seller);
+        }
+        return true;
     }
 
     function isValidErc20(address erc20) public view returns (bool) {
@@ -126,4 +103,22 @@ contract NftMarket is EIP712Upgradeable, NoncesUpgradeable {
             return false;
         }
     }
+
+    function tokenReceive(address from, address to, uint256 value, uint256 price, bytes calldata data)
+        external
+        nonReentrant
+    {
+        (address nftAddress, uint8 tokenId, uint256 deadline, uint8 v, bytes32 r, bytes32 s) =
+            abi.decode(data, (address, uint8, uint256, uint8, bytes32, bytes32));
+        if (value < price) revert NotEnoughToken(value, price);
+        require(deadline >= block.timestamp, "NFTMarket error: Invalid deadline.");
+        _permit(nftAddress, tokenId, price, deadline, v, r, s);
+        address seller = MyNft(nftAddress).ownerOf(tokenId);
+        IERC20(tokenAddress).transfer(seller, value);
+        MyNft(nftAddress).safeTransferFrom(seller, from, tokenId);
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
